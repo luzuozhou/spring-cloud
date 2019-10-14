9f13828d45cd7263a801bd5c3d5dce2edb5b84a6
huangxinlu <huangxinlu2018>
2019-10-14 18:35:43 +0800

    多种配置方式验证结果

--- hystrix-learning/hystrix-client01/src/main/java/com/luzuozhou/hystrix/client01/controller/HelloController.java
+++ hystrix-learning/hystrix-client01/src/main/java/com/luzuozhou/hystrix/client01/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.luzuozhou.hystrix.client01.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,22 +14,48 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String helloWorld() {
-        return "helloworld";
+        return "Hello World！";
     }
-    @GetMapping("/timeout")
+
+    @GetMapping("/propertiesTimeout")
     @HystrixCommand(fallbackMethod = "fallback")
-    public String timeoutTest(){
+    public String propertiesTimeout() {
         try {
-            //模拟超时熔断
+            //模拟超时
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
-            System.out.println("InterruptedException");
+            System.out.println("propertiesTimeout");
         }
         return "success";
     }
 
-    public String fallback(){
-        return "fallback";
+    @GetMapping("/annotationTimeout")
+    @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
+    public String annotationTimeout() {
+        try {
+            //模拟超时
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            System.out.println("annotationTimeout");
+        }
+        return "success";
     }
 
+    @GetMapping("/defaultTimeout")
+    @HystrixCommand(fallbackMethod = "fallback")
+    public String defaultTimeout() {
+        try {
+            //模拟超时
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            System.out.println("defaultTimeout");
+        }
+        return "success";
+    }
+
+    public String fallback() {
+        return "fallback";
+    }
 }
